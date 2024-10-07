9d88b0b0ac859a50e56f7cbc003240c8cad93a00
Harry Hathorn <hbhhathorn@gmail.com>
2024-10-07 15:45:11 +0400

    Day 2: String Calculator

---
+++ tests/MasterTDD.Day1/Instruction.md
@@ -0,0 +1,32 @@
+﻿Focus concepts:
+
+
+TDD Rules:
+
+You shall not write production code other than to pass a failing test
+You shall not write more of the production code than necessary to pass that failing test.
+You shall not write more of the test than to make it fail for the right reason.
+
+
+Triangulation - Only implement the logic once you see the pattern repeating 3 times
+
+
+Exercises:
+#1 - Leap year
+Write a function that returns true or false depending on whether its input integer is a leap year or not. 
+A leap year is defined as one that is divisible by 4, but is not otherwise divisible by 100 unless it is also divisible by 400. For example, 2001 is a typical common year and 1996 is a typical leap year, whereas 1900 is an atypical common year and 2000 is an atypical leap year.
+
+
+
+#2 Nth - Fibonacci
+Write some code to generate the Fibonacci number for the nth position ex:
+
+int Fibonacci(int position)
+
+First Fibonacci numbers in the sequence are: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+
+
+Instructions
+Upload the code to github
+Send the code in the community channel for feedback
---
+++ tests/MasterTDD.Day2/StringAdditionShould.cs
@@ -0,0 +1,98 @@
+using FluentAssertions;
+
+namespace MasterTDD.Day2
+{
+    public class StringAdditionShould
+    {
+        private readonly StringCalculator _stringCalculator;
+
+        public StringAdditionShould()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        [InlineData("1\n2,3", 6)]
+        public void CalculateSum(string input, int expected)
+        {
+            // Act
+            var result = _stringCalculator.Add(input);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(";\n", 0)]
+        [InlineData(":\n1", 1)]
+        [InlineData("|\n1|2", 3)]
+        [InlineData(" \n1\n2 3", 6)]
+        public void CalculateSum_When_DelimiterProvided(string input, int expected)
+        {
+            // Act
+            var result = _stringCalculator.Add(input);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(":\n-1", "-1")]
+        [InlineData("|\n-1|-1", "-1,-1")]
+        [InlineData(" \n1\n2 -3", "-3")]
+        public void ThrowException_When_NegativesInputted(string input, string expectedNegatives)
+        {
+            // Act & Assert
+            var exception = Assert.Throws<Exception>(() => _stringCalculator.Add(input));
+            exception.Message.Should().StartWith("negatives not allowed: ");
+            exception.Message.Should().Contain(expectedNegatives);
+        }
+    }
+
+    internal class StringCalculator
+    {
+        private const string DefaultDelimeter = ",";
+
+        public StringCalculator()
+        {
+        }
+        public int Add(string numbers)
+        {
+            int result = 0;
+            var delimeter = DefaultDelimeter;
+            var delimeterParts = numbers.Split("\n");
+            List<string> negativeNumbers = new List<string>();
+            if (delimeterParts.Length > 1 &&
+                !int.TryParse(delimeterParts[0], out _))
+            {
+                delimeter = delimeterParts[0];
+                numbers = string.Join(delimeter, delimeterParts.Skip(1));
+            }
+            if (numbers.Length > 0)
+            {
+                numbers = numbers.Replace("\n", delimeter);
+                var numberParts = numbers.Split(delimeter);
+                foreach (var numberPart in numberParts)
+                {
+                    var number = int.Parse(numberPart);
+                    if (number < 0)
+                    {
+                        negativeNumbers.Add(number.ToString());
+                    }
+                    else
+                    {
+                        result += int.Parse(numberPart);
+                    }
+                }
+            }
+            if (negativeNumbers.Any())
+            {
+                throw new Exception($"negatives not allowed: {string.Join(",", negativeNumbers)}");
+            }
+            return result;
+        }
+    }
+}
