d1287b41f54fb8855087b771edb3533aba1ea2b6
Harry Hathorn <hbhhathorn@gmail.com>
2024-10-09 09:45:40 +0400

    Day3: No change cases

--- tests/MasterTDD.Day3/UnitTest1.cs
+++ tests/MasterTDD.Day3/UnitTest1.cs
@@ -6,6 +6,16 @@ namespace MasterTDD.Day3
     {
         [Theory]
         [InlineData(100)]
+        [InlineData(50)]
+        [InlineData(20)]
+        [InlineData(10)]
+        [InlineData(5)]
+        [InlineData(1)]
+        [InlineData(0.5)]
+        [InlineData(0.25)]
+        [InlineData(0.10)]
+        [InlineData(0.05)]
+        [InlineData(0.01)]
         public void CalculateChange(decimal input, params int[] expectedChange)
         {
             int[] change = ChangeCalculator.CalculateChange(input);
