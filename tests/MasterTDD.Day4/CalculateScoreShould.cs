8a1170f56685bb39e70ea668c4e154bda075c5d4
Harry Hathorn <hbhhathorn@gmail.com>
2025-01-28 23:56:52 +0400

    Day 4: Fix wrong logic and add mix test case

--- tests/MasterTDD.Day4/CalculateScoreShould.cs
+++ tests/MasterTDD.Day4/CalculateScoreShould.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using System.ComponentModel.DataAnnotations;
 
 namespace MasterTDD.Day4
 {
@@ -6,24 +7,26 @@ namespace MasterTDD.Day4
     {
         public static TheoryData<string, int> ScoreCalculations => new()
         {
-           // spares
-           { "2/|3/|--|--|--|--|--|--|--|--||", 23 },
-           { "-/|-9|-9|-9|-9|-9|-9|-9|-9|-9||", 91 },
-           { "-1|-2|3-|5-|6-|29|59|2-|34|2/||2", 53 },
-           // no strikes
-           { "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||", 90 },
-           { "-9|-9|-9|-9|-9|-9|-9|-9|-9|-9||", 90 },
-           { "-1|-2|3-|5-|6-|29|59|2-|34|27||", 48 },
-           // strikes
-           { "X|5-|-2|--|--|--|--|--|--|--||", 24 },
-           { "X|5-|-3|X|25|-8|--|--|--|--||", 62 },
-           { "X|5-|-3|X|25|-8|X|2-|--|--||", 76 },
-           { "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5", 150 },
-           { "X|X|X|X|X|X|X|X|X|X||XX", 300 },
-           // minimum
-           { "--|--|--|--|--|--|--|--|--|--||", 0 },
-           // maximum
-           { "X|X|X|X|X|X|X|X|X|X||XX", 300 }
+            // spares
+            { "2/|3/|--|--|--|--|--|--|--|--||", 23 },
+            { "-/|-9|-9|-9|-9|-9|-9|-9|-9|-9||", 91 },
+            { "-1|-2|3-|5-|6-|29|59|2-|34|2/||2", 53 },
+            // no strikes
+            { "9-|9-|9-|9-|9-|9-|9-|9-|9-|9-||", 90 },
+            { "-9|-9|-9|-9|-9|-9|-9|-9|-9|-9||", 90 },
+            { "-1|-2|3-|5-|6-|29|59|2-|34|27||", 48 },
+            // strikes
+            { "X|5-|-2|--|--|--|--|--|--|--||", 22 },
+            { "X|5-|-3|X|25|-8|--|--|--|--||", 51 },
+            { "X|5-|-3|X|25|-8|X|2-|--|--||", 65 },
+            { "5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5", 150 },
+            { "X|X|X|X|X|X|X|X|X|X||XX", 300 },
+            // minimum
+            { "--|--|--|--|--|--|--|--|--|--||", 0 },
+            // maximum
+            { "X|X|X|X|X|X|X|X|X|X||XX", 300 },
+            // mix
+            { "X|7/|9-|X|-8|8/|-6|X|X|X||81", 167 }
         };
 
         [Theory]
@@ -36,50 +39,64 @@ namespace MasterTDD.Day4
 
         public class TinPinBowlingGame
         {
-            private static int CalculateFrame(string frame)
-            {
-                int score = 0;
-                var attempts = frame.ToCharArray();
-                foreach (var attempt in attempts)
-                {
-                    if (attempt == 'X' || attempt == '/')
-                    {
-                        score = 10;
-                        break;
-                    }
-                    else if (int.TryParse(attempt.ToString(), out int newScore)
-                        && newScore > score)
-                    {
-                        score = newScore;
-                    }
-                }
-                return score;
-            }
             public static int CalculateScore(string input)
             {
-                var parts = input.Split("||");
-                string[] turns = parts[0].Split("|");
-                var lastTwoAttempts = parts[1].Select(x => x.ToString()).ToArray();
-                var combined = turns.Concat(lastTwoAttempts).ToArray();
                 int score = 0;
-                for (int i = 0; i <= 9; i++)
+                var parts = input.Split("||");
+                var first10Attempts = parts[0].Split("|");
+                var lastTwoAttempts = parts[1].Select(x => x.ToString()).ToArray();
+                var frames = first10Attempts.Concat(lastTwoAttempts).ToArray();
+                for (int i = 0; i < 10; i++)
                 {
-                    var turn = combined[i];
-                    if (turn == "X")
+                    var frame = frames[i].Replace("-", "");
+                    if (frame == "X")
                     {
-                        score += CalculateFrame(combined[i]);
-                        score += CalculateFrame(combined[i + 1]);
-                        score += CalculateFrame(combined[i + 2]);
+                        score += 10;
+                        var next = frames[i + 1].Replace("-", "");
+                        if (next == "X")
+                        {
+                            score += 10;
+                            var nextNext = frames[i + 2][0];
+                            if (nextNext == 'X')
+                            {
+                                score += 10;
+                            }
+                            else if (nextNext != '-')
+                            {
+                                score += int.Parse(nextNext.ToString());
+                            }
+                        }
+                        else if (i == 9)
+                        {
+                            score += int.Parse(next.Last().ToString());
+                            var nextNext = frames[i + 2].Replace("-", "")[0];
+                            score += int.Parse(nextNext.ToString());
+                        }
+                        else if (next.Last() == '/')
+                        {
+                            score += 10;
+                        }
+                        else
+                        {
+                            score += int.Parse(next.Last().ToString());
+                        }
                     }
-                    else if (turn.Contains("/"))
+                    else if (frame.EndsWith("/"))
                     {
-                        score += CalculateFrame(combined[i]);
-                        var nextAttempt = combined[i + 1][0];
-                        score += CalculateFrame(nextAttempt.ToString());
+                        score += 10;
+                        var next = frames[i + 1][0];
+                        if (next == 'X')
+                        {
+                            score += 10;
+                        }
+                        else if (next != '-')
+                        {
+                            score += int.Parse(next.ToString());
+                        }
                     }
-                    else
+                    else if (!string.IsNullOrEmpty(frame))
                     {
-                        score += CalculateFrame(turn);
+                        score += int.Parse(frame.Last().ToString());
                     }
                 }
                 return score;
@@ -87,3 +104,5 @@ namespace MasterTDD.Day4
         }
     }
 }
+
+
