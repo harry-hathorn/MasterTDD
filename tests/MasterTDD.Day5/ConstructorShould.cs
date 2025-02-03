d1bfde80e484081f62981c746285eba225137c72
Harry Hathorn <hbhhathorn@gmail.com>
2025-02-03 12:41:56 +0400

    Day5: starting position

---
+++ tests/MasterTDD.Day5/ConstructorShould.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace MasterTDD.Day5
+{
+    public class ConstructorShould
+    {
+        [Fact]
+        public void SetStartingDirection() {
+            var rover = new MarsRover('N', 0, 0);
+            rover.Direction.Should().Be('N');
+        }
+    }
+}
---
+++ tests/MasterTDD.Day5/MarsRover.cs
@@ -0,0 +1,13 @@
+﻿namespace MasterTDD.Day5
+{
+    internal class MarsRover
+    {
+
+        public MarsRover(char direction, int x, int y)
+        {
+            Direction = direction;
+        }
+
+        public char Direction { get; internal set; }
+    }
+}
